e8cda2eb97888cc4e21079d8374d183e45e515df
samucaavant19 <141763952+samucaavant19@users.noreply.github.com>
2023-09-15 09:13:00 -0400

    Add files via upload

---
+++ poliformismo.cs
@@ -0,0 +1,35 @@
+public decimal CalcularVendas()
+
+  {
+
+      decimal valorUnitario = decimal.MinValue;
+
+      decimal produtosVendidos = decimal.MinValue;
+
+      return valorUnitario * produtosVendidos;
+
+  }
+  public decimal CalcularVendas()
+
+  {
+
+      decimal valorUnitario = 50;
+
+      decimal produtosVendidos = 1500;
+
+      return valorUnitario * produtosVendidos;
+
+  }
+  public decimal CalcularVendas()
+
+  {
+
+      decimal valorUnitario = 150;
+
+      decimal produtosVendidos = 3800;
+
+      decimal taxaAdicional = 100;
+
+      return taxaAdicional + (valorUnitario * produtosVendidos);
+
+  }
